e02141e70e2f0f81b8fe931f9d9e6a2deeb15160
Dylan Delobel <dylan.delobel66@gmail.com>
2016-12-26 02:53:14 +0100

    Update spawn auto upgrade

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -1,17 +1,30 @@
 module.exports = function() {
     StructureSpawn.prototype.createLargestCreep =
         function (energy, jobName) {
-            var numberOfParts = Math.floor(energy / 200);
-            var body = [];
 
-            for (let i = 0; i < numberOfParts; i++) {
-                body.push(WORK)
-            }
-            for (let i = 0; i < numberOfParts; i++) {
-                body.push(CARRY)
-            }
-            for (let i = 0; i < numberOfParts; i++) {
-                body.push(MOVE)
+            const ROLES = {
+                miner: {
+                    template: [MOVE, WORK, CARRY],
+                    templateCost: 200,
+                    upgrade: [WORK],
+                    upgradeCost: 100,
+                },
+                builder: {
+                    template: [MOVE, WORK, CARRY],
+                    templateCost: 200,
+                    upgrade: [WORK],
+                    upgradeCost: 100,
+                }
+            };
+
+            let body = [];
+
+            Array.prototype.push.apply(body, ROLES[jobName]['template']);
+
+            let energyLeft = energy - ROLES[jobName]['templateCost'];
+
+            if (energyLeft <= ROLES[jobName]['upgradeCost']) {
+                Array.prototype.push.apply(body, ROLES[jobName]['upgrade']);
             }
 
             return this.createCreep(body, undefined, { job: jobName, energyFull: false});
