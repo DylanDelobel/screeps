440a99c98b54565fef0a0a229e99286b2608c403
Dylan Delobel <dylan.delobel66@gmail.com>
2017-01-05 21:13:35 +0100

    Add creeps jobs

---
+++ creeps.jobs.js
@@ -0,0 +1,14 @@
+global.JOBS = {
+    miner: {
+        template: [MOVE, WORK, CARRY],
+        templateCost: 200,
+        upgrade: [WORK],
+        upgradeCost: 100,
+    },
+    builder: {
+        template: [MOVE, WORK, CARRY],
+        templateCost: 200,
+        upgrade: [WORK],
+        upgradeCost: 100,
+    }
+}
