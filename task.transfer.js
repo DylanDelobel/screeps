6dbca7856815fb0a0981e689517bd1161f68b35d
Dylan Delobel <dylan.delobel66@gmail.com>
2017-01-07 23:33:20 +0100

    Fix Cannot read property 'energy' of null

--- task.transfer.js
+++ task.transfer.js
@@ -1,12 +1,11 @@
 module.exports = {
-    // a function to run the logic for this role
     run: function(creep) {
-        let spawn = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+        let spawn = creep.pos.findClosestByPath(FIND_MY_SPAWNS, {
             filter: (s) => (s.structureType == STRUCTURE_SPAWN)
         });
 
         // transfer energy to spawn
-        if (spawn.energy != spawn.energyCapacity) {
+        if (spawn.energy != spawn.energyCapacity && spawn != null) {
             if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(spawn);
             }
